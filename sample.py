dc8200c67f2676932e095ae57aecfc6883544917
Ashe Liao <31528402+asheliao@users.noreply.github.com>
2019-02-21 20:49:43 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,16 +3,15 @@
 from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
 
 # Now there is a trained endpoint that can be used to make a prediction
-#prediction_key = "d03be561c2cc43a7a6845cbc58b2dbdf"
-ENDPOINT = "https://southcentralus.api.cognitive.microsoft.com"
-#project.id = "822f64ef-8867-4812-810e-6869b71ec4f4"
-#iteration.id = "94a54977-701c-461c-8461-8273afe75f6e"
 
-predictor = CustomVisionPredictionClient("d03be561c2cc43a7a6845cbc58b2dbdf", endpoint=ENDPOINT)
+ENDPOINT = "https://southcentralus.api.cognitive.microsoft.com"
+
+
+predictor = CustomVisionPredictionClient("Prediction key", endpoint=ENDPOINT)
 
 # Open the sample image and get back the prediction results.
-with open("dji1542012408189.jpg", mode="rb") as test_data:
-    results = predictor.predict_image("822f64ef-8867-4812-810e-6869b71ec4f4", test_data, "94a54977-701c-461c-8461-8273afe75f6e")
+with open("test.jpg", mode="rb") as test_data:
+    results = predictor.predict_image("Project.id", test_data)
 
 # Display the results.
 for prediction in results.predictions:
